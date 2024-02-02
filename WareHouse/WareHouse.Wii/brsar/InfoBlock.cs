92a7a678a2285216f06a4f776ef6838a0e6a46ff
shibbo <supershibbo12@gmail.com>
2024-02-02 12:29:01 -0500

    Parse BankInfo information from InfoBlock

--- WareHouse/WareHouse.Wii/brsar/InfoBlock.cs
+++ WareHouse/WareHouse.Wii/brsar/InfoBlock.cs
@@ -37,11 +37,12 @@ namespace WareHouse.Wii.brsar
             DataRef groupTableRef = new(file);
             DataRef soundArchiveRef = new(file);
 
+            /* sound data */
             file.Seek(basePos + (int)soundDataRef.GetValue());
-            int count = file.ReadInt32();
-            List<DataRef> soundInfoRefs = new(count);
+            int soundDataCount = file.ReadInt32();
+            List<DataRef> soundInfoRefs = new();
 
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < soundDataCount; i++)
             {
                 soundInfoRefs.Add(new(file));
             }
@@ -51,8 +52,25 @@ namespace WareHouse.Wii.brsar
                 file.Seek(basePos + (int)r.GetValue());
                 mSoundInfo.Add(new(file, basePos));
             }
+
+            /* bank data */
+            file.Seek(basePos + (int)soundBankRef.GetValue());
+            int soundBankCount = file.ReadInt32();
+            List<DataRef> soundBankRefs = new();
+
+            for (int i = 0; i < soundBankCount; i++)
+            {
+                soundBankRefs.Add(new(file));
+            }
+
+            foreach(DataRef r in soundBankRefs)
+            {
+                file.Seek(basePos + (int)r.GetValue());
+                mBankInfo.Add(new(file));
+            }
         }
 
         List<SoundCommonInfo> mSoundInfo = new();
+        List<BankInfo> mBankInfo = new();
     }
 }
---
+++ WareHouse/WareHouse.Wii/brsar/info/BankInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WareHouse.io;
+
+namespace WareHouse.Wii.brsar.info
+{
+    public class BankInfo
+    {
+        public BankInfo(MemoryFile file)
+        {
+            mStringID = file.ReadUInt32();
+            mFileID = file.ReadUInt32();
+            file.Skip(4);
+        }
+
+        uint mStringID;
+        uint mFileID;
+    }
+}
